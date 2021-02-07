6277687fe417b3110503247f28c3e4fdd50fbf4c
KrishnaPhalgun28 <Krishna.Phalgun28@gmail.com>
2021-02-07 14:37:02 +0530

    added snacks database

---
+++ lib/database/snacks.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:SnackApp/models/category.dart';
+
+const List<Category> snackList = const [
+  Category(
+    id: '1',
+    title: 'Cup Cake',
+    color: Colors.purple,
+  ),
+  Category(
+    id: '2',
+    title: 'Pretzels',
+    color: Colors.brown,
+  ),
+  Category(
+    id: '3',
+    title: 'Potato Chips',
+    color: Colors.red,
+  ),
+  Category(
+    id: '4',
+    title: 'Popcorn',
+    color: Colors.yellow,
+  ),
+  Category(
+    id: '5',
+    title: 'Candy Bar',
+    color: Colors.pink,
+  ),
+];
