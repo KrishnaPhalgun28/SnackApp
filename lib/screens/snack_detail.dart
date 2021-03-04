0fd27e12c049549b106f7bb1392afe005cd43ff3
KrishnaPhalgun28 <Krishna.Phalgun28@gmail.com>
2021-03-04 09:27:25 +0530

    improved theming

--- lib/screens/snack_detail.dart
+++ lib/screens/snack_detail.dart
@@ -1,14 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:SnackApp/database/snacks.dart';
 
 class SnackDetailScreen extends StatelessWidget {
   static const routeName = '/snack-detail';
 
   @override
   Widget build(BuildContext context) {
-    // final snackId = ModalRoute.of(context).settings.arguments;
-    return Center(
-      child: Text(
-        'Happy Snacking!',
+    final snackId = ModalRoute.of(context).settings.arguments;
+    final selectedSnack = snackItems.firstWhere((snack) => snack.id == snackId);
+
+    Widget buildSectionTitle(BuildContext ctx, String text) {
+      return Container(
+        margin: EdgeInsets.symmetric(
+          vertical: 10,
+        ),
+        child: Text(
+          text,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      );
+    }
+
+    Widget buildContainer(Widget child) {
+      return Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(
+            color: Colors.green,
+          ),
+        ),
+        height: 100,
+        width: 330,
+        child: child,
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${selectedSnack.title}'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 225,
+              width: double.infinity,
+              child: Image.network(
+                selectedSnack.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            buildSectionTitle(
+              context,
+              'Ingredients',
+            ),
+            buildContainer(
+              ListView.builder(
+                itemBuilder: (ctx, index) => Card(
+                  color: Theme.of(context).accentColor,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 10,
+                    ),
+                    child: Text(
+                      selectedSnack.ingredients[index],
+                    ),
+                  ),
+                ),
+                itemCount: selectedSnack.ingredients.length,
+              ),
+            ),
+            buildSectionTitle(
+              context,
+              'Steps',
+            ),
+            buildContainer(
+              ListView.builder(
+                itemBuilder: (ctx, index) => Column(
+                  children: [
+                    ListTile(
+                      leading: CircleAvatar(
+                        child: Text(
+                          '# ${index + 1}',
+                        ),
+                      ),
+                      title: Text(
+                        selectedSnack.steps[index],
+                      ),
+                    ),
+                    Divider(),
+                  ],
+                ),
+                itemCount: selectedSnack.steps.length,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
